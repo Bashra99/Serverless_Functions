09213edf27c1feeb5d12fb8e103f8571ea7b2403
Bashra99 <telfahb@gmail.com>
2022-10-31 17:54:06 +0000

    feat : add wlc massage

---
+++ api/wlc.py
@@ -0,0 +1,13 @@
+from http.server import BaseHTTPRequestHandler
+from datetime import datetime
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        massage="welcome to my world"
+        
+        self.wfile.write(massage.encode())
+        return
